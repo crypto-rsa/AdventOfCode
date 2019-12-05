8aeacc7df2b1cd419bdf87646c3dd0a67336f084
crypto_rsa <pkotrc@gmail.com>
2019-12-05 07:08:18 +0100

    Add day 2017/4

---
+++ 2017/Day4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Advent_of_Code.Year2017
+{
+    public class Day4 : IAdventDay
+    {
+        public string Name => "4. 12. 2017";
+
+        private static IEnumerable<string[]> GetInput() => System.IO.File.ReadAllLines(@"2017/Resources/day4.txt").Select(s => s.Split(' '));
+
+        public string Solve()
+        {
+            return GetInput().Count(words => words.Distinct().Count() == words.Length).ToString();
+        }
+
+        public string SolveAdvanced()
+        {
+            return GetInput().Count(words => words.Select(GetCanonicalWord).Distinct().Count() == words.Length).ToString();
+
+            string GetCanonicalWord(string word) => new string(word.OrderBy(c => c).ToArray());
+        }
+    }
+}
