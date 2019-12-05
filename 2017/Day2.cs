bf95dd3126de519eadd9f29f431f73162352d631
crypto_rsa <pkotrc@gmail.com>
2019-12-05 07:07:50 +0100

    Add day 2017/2

---
+++ 2017/Day2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent_of_Code.Year2017
+{
+    public class Day2 : IAdventDay
+    {
+        public string Name => "2. 12. 2017";
+
+        private static IEnumerable<int[]> GetInput() => System.IO.File.ReadAllLines(@"2017/Resources/day2.txt").Select(s => s.Split('\t').Select(w => int.Parse(w)).ToArray());
+
+        public string Solve()
+        {
+            return GetInput().Sum( a => a.Max() - a.Min()).ToString();
+        }
+
+        public string SolveAdvanced()
+        {
+            return GetInput().SelectMany(a => a.SelectMany((n1, i1) => a.Select((n2, i2) => i1 != i2 && n1 % n2 == 0 ? n1 / n2 : 0))).Sum().ToString();
+        }
+    }
+}
