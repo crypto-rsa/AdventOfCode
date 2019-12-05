a2dee07513012220efccf7e498cfd9c9a755cfc2
crypto_rsa <pkotrc@gmail.com>
2019-12-05 07:08:31 +0100

    Add day 2017/5

---
+++ 2017/Day5.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Advent_of_Code.Year2017
+{
+    public class Day5 : IAdventDay
+    {
+        public string Name => "5. 12. 2017";
+
+        private static int[] GetInput() => System.IO.File.ReadAllLines(@"2017/Resources/day5.txt").Select(s => int.Parse(s)).ToArray();
+
+        public string Solve()
+        {
+            var indices = GetInput();
+            int curIndex = 0;
+            int steps = 0;
+
+            while( curIndex < indices.Length )
+            {
+                curIndex += ++indices[curIndex] - 1;
+                steps++;
+            }
+
+            return steps.ToString();
+        }
+
+        public string SolveAdvanced()
+        {
+            var indices = GetInput();
+            int curIndex = 0;
+            int steps = 0;
+
+            while( curIndex < indices.Length )
+            {
+                int offset = indices[curIndex];
+                indices[curIndex] += offset >= 3 ? -1 : +1;
+                curIndex += offset;
+                steps++;
+            }
+
+            return steps.ToString();
+        }
+    }
+}
