df6dd850fc6ba3a4f7893ead0fd665525b44a7a3
crypto_rsa <pkotrc@gmail.com>
2020-12-05 10:30:44 +0100

    Add day 2020/5

---
+++ 2020/Day5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Advent_of_Code.Year2020
+{
+    public class Day5 : IAdventDay
+    {
+        public string Name => "5. 12. 2020";
+
+        private static string[] GetInput() => System.IO.File.ReadAllLines("2020/Resources/day5.txt");
+
+        public string Solve() => GetInput().Max(ToNumber).ToString();
+
+        public string SolveAdvanced()
+        {
+            var allNumbers = GetInput().Select(ToNumber).ToHashSet();
+            var number = Enumerable.Range(0, 1024).Single(i => !allNumbers.Contains(i) && allNumbers.Contains(i - 1) && allNumbers.Contains(i + 1));
+
+            return number.ToString();
+        }
+
+        private static int ToNumber(string input)
+        {
+            var binary = input.Replace('F', '0').Replace('B', '1').Replace('L', '0').Replace('R', '1');
+
+            return Convert.ToInt32(binary[0..7], 2) * 8 + Convert.ToInt32(binary[7..10], 2);
+        }
+    }
+}
