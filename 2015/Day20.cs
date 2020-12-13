9fa0a43706a2445f93e3c2b2a5f539fbd22c14fd
crypto_rsa <pkotrc@gmail.com>
2020-12-13 12:47:05 +0100

    Add day 2015/20

---
+++ 2015/Day20.cs
@@ -0,0 +1,45 @@
+﻿namespace Advent_of_Code.Year2015
+{
+    public class Day20 : IAdventDay
+    {
+        public string Name => "20. 12. 2015";
+
+        private const int Input = 36000000;
+
+        public string Solve() => GetNumberWithDivisorSum(10, int.MaxValue);
+
+        public string SolveAdvanced() => GetNumberWithDivisorSum(11, 50);
+
+        private static string GetNumberWithDivisorSum(int presentsPerHouse, int maxHouses)
+        {
+            long upperBound = Input / presentsPerHouse;
+
+            var divisors = new long[upperBound + 1];
+            long minimum = long.MaxValue;
+
+            for (long d = 1; d <= upperBound; d++)
+            {
+                for (long i = d, k = 1; k <= maxHouses && i <= upperBound && i <= minimum; i += d, k++)
+                {
+                    divisors[i] += presentsPerHouse * d;
+
+                    if (divisors[i] >= Input)
+                    {
+                        if (i < minimum)
+                        {
+                            minimum = i;
+                        }
+
+                        if (d >= minimum)
+                            break;
+                    }
+                }
+
+                if (d >= minimum)
+                    return minimum.ToString();
+            }
+
+            return string.Empty;
+        }
+    }
+}
