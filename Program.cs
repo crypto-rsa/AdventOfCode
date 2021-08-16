aba62232c3b0d36be99f3e2f406cb23d2b6b0d1a
crypto_rsa <pkotrc@gmail.com>
2021-08-16 08:51:17 +0200

    Run the program through command line arguments

--- Program.cs
+++ Program.cs
@@ -1,16 +1,43 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 
 namespace Advent_of_Code
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: Advent_of_Code <year> <day>");
+                return;
+            }
+
+            var day = GetAdventDay();
+
+            if (day == null)
+            {
+                Console.WriteLine($"Cannot find day {args[1]} for year {args[0]}!");
+                return;
+            }
+
             var stopWatch = System.Diagnostics.Stopwatch.StartNew();
-            IAdventDay day = new Advent_of_Code.Year2018.Day19();
 
             Console.WriteLine($"{day.Name}: {day.Solve()}, {day.SolveAdvanced()}");
             Console.WriteLine($"Calculated in {stopWatch.ElapsedMilliseconds} ms");
+
+            IAdventDay GetAdventDay()
+            {
+                string fullName = $"Advent_of_Code.Year{args[0]}.Day{args[1]}";
+
+                var type = Assembly.GetExecutingAssembly().GetTypes().FirstOrDefault(t => t.FullName == fullName);
+
+                if (type == null)
+                    return null;
+
+                return Activator.CreateInstance(type) as IAdventDay;
+            }
         }
     }
 }
