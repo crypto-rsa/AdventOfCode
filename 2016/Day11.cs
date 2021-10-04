3b9867da502b0b967109bb2b79904d12ef6f54a7
crypto_rsa <pkotrc@gmail.com>
2021-10-04 10:28:07 +0200

    Move class Heap to namespace Tools

--- 2016/Day11.cs
+++ 2016/Day11.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Tools;
 
 namespace Advent_of_Code.Year2016
 {
@@ -100,98 +101,6 @@ namespace Advent_of_Code.Year2016
                 return HashCode.Combine(ElevatorPosition, (ushort)Floor1 | (Floor2 << 16), (ushort)Floor3 | (Floor4 << 16));
             }
         }
-
-        private readonly struct HeapItem<T>
-        {
-            public HeapItem(T element, int value)
-            {
-                Element = element;
-                Value = value;
-            }
-
-            public T Element { get; }
-
-            public int Value { get; }
-        }
-
-        private class Heap<T>
-        {
-            private readonly List<HeapItem<T>> _list = new() { default };
-
-            public int Count => _list.Count - 1;
-
-            public void Push(T element, int value)
-            {
-                _list.Add(new HeapItem<T>( element, value));
-                SiftUp(_list.Count - 1);
-            }
-
-            public T Pop()
-            {
-                T result = _list[1].Element;
-                _list[1] = _list[^1];
-                _list.RemoveAt(_list.Count - 1);
-                SiftDown(1);
-
-                return result;
-            }
-
-            private static int Parent(int i) => i / 2;
-            private static int Left(int i) => i * 2;
-            private static int Right(int i) => i * 2 + 1;
-
-            private void SiftUp(int index)
-            {
-                int current = index;
-
-                while (current > 1)
-                {
-                    int parent = Parent(current);
-
-                    if (_list[current].Value > _list[parent].Value)
-                        return;
-
-                    (_list[parent], _list[current]) = (_list[current], _list[parent]);
-                    current = parent;
-                }
-            }
-
-            private void SiftDown(int index)
-            {
-                int current = index;
-
-                while(current < _list.Count)
-                {
-                    int smallest = GetSmallest(current);
-
-                    if (smallest == current)
-                        return;
-
-                    (_list[current], _list[smallest]) = (_list[smallest], _list[current]);
-                    current = smallest;
-                }
-
-                int GetSmallest(int root)
-                {
-                    int left = Left(root);
-                    int right = Right(root);
-
-                    if (GetSmaller(root, left) == root && GetSmaller(root, right) == root)
-                        return root;
-
-                    return GetSmaller(left, right);
-                }
-
-                int GetSmaller(int i, int j)
-                {
-                    int iValue = i < _list.Count ? _list[i].Value : int.MaxValue;
-                    int jValue = j < _list.Count ? _list[j].Value : int.MaxValue;
-
-                    return iValue <= jValue ? i : j;
-                }
-            }
-        }
-
 
         private class Search
         {
---
+++ Tools/Heap.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+
+namespace Tools
+{
+    internal readonly struct HeapItem<T>
+    {
+        public HeapItem(T element, int value)
+        {
+            Element = element;
+            Value = value;
+        }
+
+        public T Element { get; }
+
+        public int Value { get; }
+    }
+
+    internal class Heap<T>
+    {
+        private readonly List<HeapItem<T>> _list = new() { default };
+
+        public int Count => _list.Count - 1;
+
+        public void Push(T element, int value)
+        {
+            _list.Add(new HeapItem<T>(element, value));
+            SiftUp(_list.Count - 1);
+        }
+
+        public T Pop()
+        {
+            T result = _list[1].Element;
+            _list[1] = _list[^1];
+            _list.RemoveAt(_list.Count - 1);
+            SiftDown(1);
+
+            return result;
+        }
+
+        private static int Parent(int i) => i / 2;
+        private static int Left(int i) => i * 2;
+        private static int Right(int i) => i * 2 + 1;
+
+        private void SiftUp(int index)
+        {
+            int current = index;
+
+            while (current > 1)
+            {
+                int parent = Parent(current);
+
+                if (_list[current].Value > _list[parent].Value)
+                    return;
+
+                (_list[parent], _list[current]) = (_list[current], _list[parent]);
+                current = parent;
+            }
+        }
+
+        private void SiftDown(int index)
+        {
+            int current = index;
+
+            while (current < _list.Count)
+            {
+                int smallest = GetSmallest(current);
+
+                if (smallest == current)
+                    return;
+
+                (_list[current], _list[smallest]) = (_list[smallest], _list[current]);
+                current = smallest;
+            }
+
+            int GetSmallest(int root)
+            {
+                int left = Left(root);
+                int right = Right(root);
+
+                if (GetSmaller(root, left) == root && GetSmaller(root, right) == root)
+                    return root;
+
+                return GetSmaller(left, right);
+            }
+
+            int GetSmaller(int i, int j)
+            {
+                int iValue = i < _list.Count ? _list[i].Value : int.MaxValue;
+                int jValue = j < _list.Count ? _list[j].Value : int.MaxValue;
+
+                return iValue <= jValue ? i : j;
+            }
+        }
+    }
+}
